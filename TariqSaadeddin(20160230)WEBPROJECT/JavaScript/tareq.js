2aa28e07c3690c11abe43486c1f16bdca3c3c5d7
TariqSaadeddin <66224673+TariqSaadeddin@users.noreply.github.com>
2020-06-01 00:49:22 +0300

    Add files via upload

---
+++ TariqSaadeddin(20160230)WEBPROJECT/JavaScript/tareq.js
@@ -0,0 +1,19 @@
+function welcomeMsg(){
+	var x=prompt("What is your name?");
+	document.write("Hello " + x + ", welcome to my page");
+}
+function Education(){
+alert("this page includes my education");
+}
+function Certificates(){
+alert("these are the certificates I've earned");
+}
+function Experiences(){
+	alert("welcome to my experience page");
+}
+function Contact(){
+	alert("Feel free to contact me anytime");
+}
+function Skills(){
+	alert("this page contains my skills");
+}
